11f7515c1dbf42aed1c4f029e67e2872912243bd
Alireza Savand <alireza.savand@gmail.com>
2011-11-27 17:51:27 +0400

    Set on_preferencesAction_triggered() to show Preferences

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "preferencesdialog.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,12 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_preferencesAction_triggered()
+{
+    PreferencesDialog prefDialog;
+    if(prefDialog.exec() == QDialog::Accepted) {
+        // Doing some
+    }
 }

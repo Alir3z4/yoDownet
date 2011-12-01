01629af101ff684d2ae01ffa4feb61f43d8f05c5
Alireza Savand <alireza.savand@gmail.com>
2011-12-01 05:57:04 +0400

    protected memeber initialed

--- abstractdownloader.h
+++ abstractdownloader.h
@@ -8,11 +8,18 @@ class AbstractDownloader : public QObject
     Q_OBJECT
 public:
     explicit AbstractDownloader(QObject *parent = 0);
-    
-signals:
-    
-public slots:
-    
+
+protected:
+    // Holding Url, this one shoulda huged tightly
+    // Because the main business is url:x
+    QString url;
+    // Then shoulda wink File name
+    QString fileName;
+    // Excusme, Where do you wanna go?
+    QString fileDestPath;
+    // Okay then, don't be shy my file
+    // Just wanna know your size
+    int fileSize;
 };
 
 #endif // ABSTRACTDOWNLOADER_H

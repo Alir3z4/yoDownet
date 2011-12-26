95f81552c5c0d56db9f144a0264f5ca4c6fa82b4
Alireza Savand <alireza.savand@gmail.com>
2011-12-26 22:24:34 +0400

    make Save to lineEdit work

--- prefdownloaderswidget.cpp
+++ prefdownloaderswidget.cpp
@@ -6,6 +6,10 @@ PrefDownloadersWidget::PrefDownloadersWidget(QWidget *parent) :
     ui(new Ui::PrefDownloadersWidget)
 {
     ui->setupUi(this);
+
+    // TODO: get set last selected save to directory path from saved setting [INI files] with QSettings
+    ui->saveToEdit->setCaption(tr("Select a directory to save downloaded files"));
+    ui->saveToEdit->setBrowzType(FileBrowz::GetDirectory);
 }
 
 PrefDownloadersWidget::~PrefDownloadersWidget()

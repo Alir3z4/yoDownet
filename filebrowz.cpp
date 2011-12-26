3b0f84737eef8d16f054b5e50cbf2e5193a4ac88
Alireza Savand <alireza.savand@gmail.com>
2011-12-26 22:24:16 +0400

    FileBrowz designed and implemented now it's tottaly basic but works properly as expect

--- filebrowz.cpp
+++ filebrowz.cpp
@@ -6,9 +6,32 @@ FileBrowz::FileBrowz(QWidget *parent) :
     ui(new Ui::FileBrowz)
 {
     ui->setupUi(this);
+
+    // make GetFileName mode to default
+    setBrowzType(GetFileName);
+    setOptions(0);
 }
 
 FileBrowz::~FileBrowz()
 {
     delete ui;
+}
+
+void FileBrowz::on_browzButton_clicked()
+{
+    QString result;
+
+    if(browzType() == GetDirectory) {
+        result = QFileDialog::getExistingDirectory(
+                    this, caption(), ui->pathEdit->text());
+
+    } else if(browzType() == GetFileName) {
+        result = QFileDialog::getOpenFileName(this, caption(), ui->pathEdit->text(), filter());
+    }
+
+    if(!result.isEmpty()) {
+        QString oldFile = ui->pathEdit->text();
+        ui->pathEdit->setText(result);
+        if (oldFile != result) emit fileChanged(result);
+    }
 }
--- filebrowz.h
+++ filebrowz.h
@@ -1,7 +1,7 @@
 #ifndef FileBrowz_H
 #define FileBrowz_H
 
-#include <QWidget>
+#include <QFileDialog>
 
 namespace Ui {
 class FileBrowz;
@@ -12,11 +12,37 @@ class FileBrowz : public QWidget
     Q_OBJECT
     
 public:
+    enum BrowzType {
+        GetFileName,
+        GetDirectory
+    };
+
     explicit FileBrowz(QWidget *parent = 0);
     ~FileBrowz();
+
+    inline QString caption() const { return _caption; }
+    inline QString filter() const { return _filter; }
+    inline BrowzType browzType() const { return _type; }
+    inline QFileDialog::Options options() const { return _options; }
+
+public slots:
+    inline void setCaption(const QString &caption) { _caption = caption; }
+    inline void setFilter(const QString &filter) { _filter = filter; }
+    inline void setBrowzType(BrowzType type) { _type = type; }
+    inline void setOptions(QFileDialog::Options options) { _options = options; }
     
+private slots:
+    void on_browzButton_clicked();
+
+signals:
+    void fileChanged(QString file);
+
 private:
     Ui::FileBrowz *ui;
+    QString _caption;
+    QString _filter;
+    BrowzType _type;
+    QFileDialog::Options _options;
 };
 
 #endif // FileBrowz_H

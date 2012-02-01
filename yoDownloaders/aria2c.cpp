235b6be45e7d1913ca78bf857956b0ea6ac80915
Alireza Savand <alireza.savand@gmail.com>
2012-02-01 04:21:47 +0400

    tellStatus added this is gonna tell yoDownload everything about current uri

--- yoDownloaders/aria2c.cpp
+++ yoDownloaders/aria2c.cpp
@@ -1,62 +1,28 @@
 #include "aria2c.h"
+#include "yoUtils/fileinfo.h"
+#include "yoUtils/fault.h"
+#include "yoUtils/uristatus.h"
 
 Aria2c::Aria2c(std::string url)
     :_client(&_transport), _carriageParm(url)
 {
 }
 
-const QString Aria2c::addUri(const QVector<QString> &q_uris)
+const QString Aria2c::addUri(const QVector<QString> &qUris)
 {
-    std::vector<std::string> uris = fromQVector(q_uris);
+    std::vector<std::string> uris = fromQVector(qUris);
 
     std::string const methodName = "aria2.addUri";
     xmlrpc_c::value result;
     xmlrpc_c::paramList params;
-    std::vector<xmlrpc_c::value> v_uris;
+    std::vector<xmlrpc_c::value> vUris;
 
     for (int i = 0, j = uris.size(); i < j; i++)
-        v_uris.push_back(xmlrpc_c::value_string(uris[i].c_str()));
+        vUris.push_back(xmlrpc_c::value_string(uris[i].c_str()));
 
-    xmlrpc_c::value_array a_uris(v_uris);
+    xmlrpc_c::value_array aUris(vUris);
 
-    params.add(a_uris);
-
-    xmlrpc_c::rpcPtr rpc(methodName, params);
-    rpc->call(&_client, &_carriageParm);
-
-    if(rpc->isSuccessful())
-        result = rpc->getResult();
-    throw rpc->getFault();
-
-    return QString::fromStdString(xmlrpc_c::value_string(result));
-}
-
-const QString Aria2c::addUri(const QVector<QString> &q_uris, const QMap<QString, QString> &q_options)
-{
-    std::vector<std::string> uris = fromQVector(q_uris);
-    std::map<std::string, std::string> options = fromQMap(q_options);
-
-    std::string const methodName = "aria2.addUri";
-    xmlrpc_c::value result;
-    xmlrpc_c::paramList params;
-    std::vector<xmlrpc_c::value> v_uris;
-
-    for (int i = 0, j = uris.size(); i < j; i++)
-        v_uris.push_back((xmlrpc_c::value_string(uris[i].c_str())));
-
-    xmlrpc_c::value_array a_uris(v_uris);
-    std::map<std::string, xmlrpc_c::value> m_options;
-
-    for(std::map<std::string, std::string>::iterator iter = options.begin(); iter != options.end(); iter++)
-    {
-        std::pair<std::string, xmlrpc_c::value> p_option(iter->first, xmlrpc_c::value_string(iter->second));
-        m_options.insert(p_option);
-    }
-
-    xmlrpc_c::value_struct s_options(m_options);
-
-    params.add(a_uris);
-    params.add(s_options);
+    params.add(aUris);
 
     xmlrpc_c::rpcPtr rpc(methodName, params);
     rpc->call(&_client, &_carriageParm);
@@ -64,7 +30,45 @@ const QString Aria2c::addUri(const QVector<QString> &q_uris, const QMap<QString,
     if(rpc->isSuccessful())
         result = rpc->getResult();
     else
-        throw rpc->getFault();
+        throwException(rpc);
+
+    return QString::fromStdString(xmlrpc_c::value_string(result));
+}
+
+const QString Aria2c::addUri(const QVector<QString> &qUris, const QMap<QString, QString> &qOptions)
+{
+    std::vector<std::string> uris = fromQVector(qUris);
+    std::map<std::string, std::string> options = fromQMap(qOptions);
+
+    std::string const methodName = "aria2.addUri";
+    xmlrpc_c::value result;
+    xmlrpc_c::paramList params;
+    std::vector<xmlrpc_c::value> vUris;
+
+    for (int i = 0, j = uris.size(); i < j; i++)
+        vUris.push_back((xmlrpc_c::value_string(uris[i].c_str())));
+
+    xmlrpc_c::value_array aUris(vUris);
+    std::map<std::string, xmlrpc_c::value> mOptions;
+
+    for(std::map<std::string, std::string>::iterator iter = options.begin(); iter != options.end(); iter++)
+    {
+        std::pair<std::string, xmlrpc_c::value> pOption(iter->first, xmlrpc_c::value_string(iter->second));
+        mOptions.insert(pOption);
+    }
+
+    xmlrpc_c::value_struct sOptions(mOptions);
+
+    params.add(aUris);
+    params.add(sOptions);
+
+    xmlrpc_c::rpcPtr rpc(methodName, params);
+    rpc->call(&_client, &_carriageParm);
+
+    if(rpc->isSuccessful())
+        result = rpc->getResult();
+    else
+        throwException(rpc);
 
     return QString::fromStdString(xmlrpc_c::value_string(result));
 }
@@ -84,7 +88,7 @@ const QString Aria2c::remove(const QString q_gid)
     if(rpc->isSuccessful())
         result = rpc->getResult();
     else
-        throw rpc->getFault();
+        throwException(rpc);
 
     return QString::fromStdString(xmlrpc_c::value_string(result));
 }
@@ -172,6 +176,186 @@ const DownloaderVersion* Aria2c::getVersion()
     return version;
 }
 
+const Status *Aria2c::tellStatus(const QString q_gid)
+{
+    std::string const methodName = "aria2.tellStatus";
+    std::string gid = q_gid.toStdString();
+    xmlrpc_c::value result;
+
+    xmlrpc_c::paramList params;
+    params.add(xmlrpc_c::value_string(gid));
+
+    xmlrpc_c::rpcPtr rpc(methodName, params);
+
+    rpc->call(&_client, &_carriageParm);
+
+    if(rpc->isSuccessful())
+        result = rpc->getResult();
+    else
+        throwException(rpc);
+
+    // So here's the movie story
+    // the result type now is an XML struct
+    // Shoulda to be converted to C++ map ;)
+    // This is gonna be fun! [ actually noth'n funny here, just eeeeh!!! ]
+    xmlrpc_c::value_struct structResult(result);
+    std::map<std::string, xmlrpc_c::value> mapResult(
+                static_cast< std::map<std::string, xmlrpc_c::value> >(structResult));
+
+    Status *status = NULL;
+
+    std::map<std::string, xmlrpc_c::value>::iterator itProp;
+
+    itProp = mapResult.find("status");
+    if(itProp != mapResult.end())
+        status->setStatus(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("gid");
+    if(itProp != mapResult.end())
+        status->setGid(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("totalLength");
+    if(itProp != mapResult.end())
+        status->setTotalLength(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("completedLength");
+    if(itProp != mapResult.end())
+        status->setCompletedLength(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("uploadLength");
+    if(itProp != mapResult.end())
+        status->setUploadLength(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("bitField");
+    if(itProp != mapResult.end())
+        status->setBitField(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("downloadSpeed");
+    if(itProp != mapResult.end())
+        status->setDownloadSpeed(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("uploadSpeed");
+    if(itProp != mapResult.end())
+        status->setUploadSpeed(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("infoHash");
+    if(itProp != mapResult.end())
+        status->setInfoHash(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("numSeeders");
+    if(itProp != mapResult.end())
+        status->setNumSeeders(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("pieceLength");
+    if(itProp != mapResult.end())
+        status->setPieceLength(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("numPieces");
+    if(itProp != mapResult.end())
+        status->setNumPieces(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("connections");
+    if(itProp != mapResult.end())
+        status->setConnections(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("errorCode");
+    if(itProp != mapResult.end())
+        status->setErrorCode(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("followedBy");
+    if(itProp != mapResult.end())
+        status->setFollowedBy(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("belongsTo");
+    if(itProp != mapResult.end())
+        status->setBelongsTo(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("dir");
+    if(itProp != mapResult.end())
+        status->setDir(QString::fromStdString(xmlrpc_c::value_string(itProp->second)));
+
+    itProp = mapResult.find("files");
+    if(itProp != mapResult.end()){
+        // So basicaly we cast it to a 'vector'
+        std::vector<xmlrpc_c::value> vFiles(xmlrpc_c::value_array(itProp->second).vectorValueValue());
+        // Then we create an iterator for the 'vector'
+        std::vector<xmlrpc_c::value>::iterator itFiles = vFiles.begin();
+
+        // So here fo each fuck'n 'files'
+        for(; itFiles != vFiles.end(); itFiles++) {
+            // Convert it to an xml struct
+            xmlrpc_c::value_struct structFile(*itFiles);
+            // Then to a C++ map
+            std::map<std::string, xmlrpc_c::value> mapFile(
+                        static_cast< std::map<std::string, xmlrpc_c::value> > (structFile));
+
+            std::map<std::string, xmlrpc_c::value>::iterator itFile;
+            FileInfo *tempFileInfo = NULL;
+
+            itFile = mapFile.find("index");
+            if(itFile != mapFile.end())
+                tempFileInfo->setIndex(QString::fromStdString(xmlrpc_c::value_string(itFile->second)));
+
+            itFile = mapFile.find("path");
+            if(itFile != mapFile.end())
+                tempFileInfo->setPath(QString::fromStdString(xmlrpc_c::value_string(itFile->second)));
+
+            itFile = mapFile.find("length");
+            if(itFile != mapFile.end())
+                tempFileInfo->setLength(QString::fromStdString(xmlrpc_c::value_string(itFile->second)));
+
+            itFile = mapFile.find("selected");
+            if(itFile != mapFile.end())
+                tempFileInfo->setSelected(QString::fromStdString(xmlrpc_c::value_string(itFile->second)));
+
+            // Then an 'array' of uri's
+            // :|
+            itFile = mapFile.find("uris");
+            if(itFile != mapFile.end()){
+                // So it's old
+                // Again dealing with an xml array
+                // then converting to c++ map :|
+                std::vector<xmlrpc_c::value> vUris(xmlrpc_c::value_array(itFile->second).vectorValueValue());
+                std::vector<xmlrpc_c::value>::iterator itUris = vUris.begin();
+                for(; itUris != vUris.end(); itUris++){
+                    xmlrpc_c::value_struct structUri(*itUris);
+                    std::map<std::string, xmlrpc_c::value> mapUri(
+                                static_cast< std::map<std::string, xmlrpc_c::value> > (structUri));
+
+                    std::map<std::string, xmlrpc_c::value>::iterator itUri;
+
+                    UriStatus *tempUriStatus;
+
+                    itUri = mapUri.find("uri");
+                    if(itUri != mapUri.end())
+                        tempUriStatus->setUri(QString::fromStdString(xmlrpc_c::value_string(itUri->second)));
+
+                    itUri = mapUri.find("status");
+                    if(itUri != mapUri.end())
+                        tempUriStatus->setStatus(QString::fromStdString(xmlrpc_c::value_string(itUri->second)));
+
+                    QVector<UriStatus*> tempUriStatusVector;
+                    tempUriStatusVector.push_back(tempUriStatus);
+
+                    tempFileInfo->setUris(tempUriStatusVector);
+                }
+            }
+
+            QVector<FileInfo*> tempFileInfoVector;
+            tempFileInfoVector.push_back(tempFileInfo);
+
+            status->setFiles(tempFileInfoVector);
+        }
+    }
+
+    itProp = mapResult.find("bittorrent");
+    if(itProp != mapResult.end()){
+        // TODO: add bittorrent information to "status"
+    }
+
+    return status;
+}
+
 const QString Aria2c::shutdown()
 {
     return _forDoneSake("aria2.shutdown", QString());
@@ -182,10 +366,11 @@ const QString Aria2c::forceShutdown()
     return _forDoneSake("aria2.forceShutdown", QString());
 }
 
-const QString Aria2c::_forDoneSake(const QString q_methodName, const QString q_gid = "")
+const QString Aria2c::purgeDownloadResult()
 {
-    std::string methodName = q_methodName.toStdString();
-    std::string gid = q_gid.toStdString();
+    return _forDoneSake("aria2.purgeDownloadResult", QString());
+}
+
 void Aria2c::throwException(const xmlrpc_c::rpcPtr &rpc)
 {
     xmlrpc_c::fault tempFault;
@@ -199,6 +384,10 @@ void Aria2c::throwException(const xmlrpc_c::rpcPtr &rpc)
     throw fault;
 }
 
+const QString Aria2c::_forDoneSake(const QString qMethodname, const QString qGid = "")
+{
+    std::string methodName = qMethodname.toStdString();
+    std::string gid = qGid.toStdString();
     xmlrpc_c::value result;
     xmlrpc_c::paramList params; // Just initial, not used :D
 
@@ -216,23 +405,23 @@ void Aria2c::throwException(const xmlrpc_c::rpcPtr &rpc)
     return QString::fromStdString(xmlrpc_c::value_string(result));
 }
 
-const std::vector<std::string> Aria2c::fromQVector(const QVector<QString> &q_vect)
+const std::vector<std::string> Aria2c::fromQVector(const QVector<QString> &qVect)
 {
-    std::vector<std::string> std_vect;
-    for (int i = 0; i < q_vect.size(); ++i) {
-        std_vect.push_back(q_vect[i].toStdString());
+    std::vector<std::string> stdVect;
+    for (int i = 0; i < qVect.size(); ++i) {
+        stdVect.push_back(qVect[i].toStdString());
     }
-    return std_vect;
+    return stdVect;
 }
 
-const std::map<std::string, std::string> Aria2c::fromQMap(const QMap<QString, QString> &q_map)
+const std::map<std::string, std::string> Aria2c::fromQMap(const QMap<QString, QString> &qMap)
 {
-    std::map<std::string, std::string> std_map;
-    QMap<QString, QString>::const_iterator i = q_map.constBegin();
-    while (i != q_map.constEnd()) {
-        std_map.insert(std::pair<std::string, std::string>(i.key().toStdString(), i.value().toStdString()));
+    std::map<std::string, std::string> stdMap;
+    QMap<QString, QString>::const_iterator i = qMap.constBegin();
+    while (i != qMap.constEnd()) {
+        stdMap.insert(std::pair<std::string, std::string>(i.key().toStdString(), i.value().toStdString()));
     }
 
-    return std_map;
+    return stdMap;
 }
 
--- yoDownloaders/aria2c.h
+++ yoDownloaders/aria2c.h
@@ -1,13 +1,12 @@
 #ifndef ARIA2C_H
 #define ARIA2C_H
 
-#include <vector>
-#include <string>
-#include <map>
-#include <xmlrpc-c/client_simple.hpp>
 #include <QVector>
 #include <QMap>
 #include <QString>
+#include <xmlrpc-c/client_simple.hpp>
+#include "yoUtils/downloaderversion.h"
+#include "yoUtils/status.h"
 
 class Aria2c
 {
@@ -15,7 +14,7 @@ public:
     Aria2c(std::string url);
 
     // Operations
-    const QString addUri(const QVector<QString> &q_uris);
+    const QString addUri(const QVector<QString> &qUris);
     const QString addUri(const QVector<QString> &q_uris, const QMap<QString, QString> &q_options);
 
     // Aria2c actions
@@ -32,10 +31,24 @@ public:
 
     // This tricky guy, version :~]
     const DownloaderVersion* getVersion();
+    // This method returns download progress of the download denoted by gid. gid is of type string
+    // keys is array of string. If it is specified, the response contains only keys in keys array.
+    // If keys is empty or not specified, the response contains all keys.
+    // This is useful when you just want specific keys and avoid unnecessary transfers.
+    // For example, aria2.tellStatus("1", ["gid", "status"]) returns gid and status key.
+    // The response is of type struct and it contains following keys. The value type is string.
+    // ref: http://aria2.sourceforge.net/aria2c.1.html#aria2_rpc_aria2_tellStatus
+    const Status* tellStatus(const QString q_gid);
+
     // Shutdown :|
     const QString shutdown();
     // Shut da fuck up, will ya!
     const QString forceShutdown();
+
+    //Purges completed/error/removed downloads to free memory.
+    //This method returns "OK"
+    const QString purgeDownloadResult();
+
 
 private:
     void throwException(const xmlrpc_c::rpcPtr &rpc);
@@ -45,16 +58,16 @@ private:
     xmlrpc_c::carriageParm_curl0 _carriageParm;
 
     // make some noise :d
-    const QString _forDoneSake(const QString q_methodName, const QString q_gid);
+    const QString _forDoneSake(const QString qMethodname, const QString qGid);
 
     // fuck the disaster with this QVector<QString> => std::vector<string>
-    // actually i opened an topic for it on [ qtcentre.org/threads/47074 ], but i can't w8 till this fellas
+    // actually i opened a topic for it on [ qtcentre.org/threads/47074 ], but i can't w8 till this fellas
     // got what my point is
     // if i be success in it in future i'll take it away
     // but i think it's better to keep conversion between qvector->vector here in this aria2c class :|
     // who knows ?
-    const std::vector<std::string> fromQVector(const QVector<QString> &q_vect);
-    const std::map<std::string, std::string> fromQMap(const QMap<QString, QString> &q_map);
+    const std::vector<std::string> fromQVector(const QVector<QString> &qVect);
+    const std::map<std::string, std::string> fromQMap(const QMap<QString, QString> &qMap);
 };
 
 #endif // ARIA2C_H

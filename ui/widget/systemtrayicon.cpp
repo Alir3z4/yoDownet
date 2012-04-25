cc468e3e156e6176ff8c77fe370a7e397d3a77b0
Alireza Savand <alireza.savand@gmail.com>
2012-04-25 04:08:28 +0400

    implement basics on SystemTryIcon

--- ui/widget/systemtrayicon.cpp
+++ ui/widget/systemtrayicon.cpp
@@ -3,4 +3,21 @@
 SystemTrayIcon::SystemTrayIcon(QWidget *parent) :
     QWidget(parent)
 {
+    connect(this, SIGNAL(ready()), this, SLOT(showTryIcon()));
+}
+
+void SystemTrayIcon::setReady(const QList<QAction *> &actions, const QIcon &icon)
+{
+    _tryIcon = new QSystemTrayIcon;
+    _tryIconMenu = new QMenu;
+
+    _tryIconMenu->addActions(actions);
+    _tryIcon->setContextMenu(_tryIconMenu);
+    _tryIcon->setIcon(icon);
+    emit ready();
+}
+
+void SystemTrayIcon::showTryIcon()
+{
+    _tryIcon->show();
 }
--- ui/widget/systemtrayicon.h
+++ ui/widget/systemtrayicon.h
@@ -2,17 +2,27 @@
 #define SYSTEMTRAYICON_H
 
 #include <QWidget>
+#include <QSystemTrayIcon>
+#include <QMenu>
 
 class SystemTrayIcon : public QWidget
 {
     Q_OBJECT
 public:
     explicit SystemTrayIcon(QWidget *parent = 0);
-    
+
+    void setReady(const QList<QAction *> &actions, const QIcon &icon);
+
 signals:
-    
-public slots:
-    
+    void ready();
+
+private slots:
+    void showTryIcon();
+
+private:
+    QSystemTrayIcon *_tryIcon;
+    QMenu *_tryIconMenu;
+
 };
 
 #endif // SYSTEMTRAYICON_H

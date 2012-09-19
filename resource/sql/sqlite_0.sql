32fe2621e71632bc2f3b9ce07d239450d14da5ab
Alireza Savand <alireza.savand@gmail.com>
2012-09-20 02:11:06 +0400

    Removed sql files
    From now on, there is no sql shit anymore

--- resource/sql/sqlite_0.sql
+++
@@ -1,6 +0,0 @@
-------------------------------------------------------------------------------------------
--- sqlite_0.sql is part of yoDownet
--- Copyright 2011, 2012 Alireza Savand <alireza.savand@gmail.com>
--- subject to terms and condition provided in COPYING
-------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS "urls" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL ,"url" TEXT,"save_path" TEXT,"status" INTEGER,"progress" INTEGER,"remaining_time" VARCHAR(75) DEFAULT "n/a","speed" VARCHAR(75),"created_at" TIMESTAMP NOT NULL  DEFAULT CURRENT_TIMESTAMP,"updated_at" TIMESTAMP);
--- resource/sql/sqlite_1.sql
+++
@@ -1,6 +0,0 @@
-------------------------------------------------------------------------------------------
--- sqlite_1.sql is part of yoDownet
--- Copyright 2012 Alireza Savand <alireza.savand@gmail.com>
--- subject to terms and condition provided in COPYING
-------------------------------------------------------------------------------------------
-ALTER TABLE "urls" ADD COLUMN "filename" VARCHAR;

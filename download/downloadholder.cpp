487da87174896b9e32ee235f3ad1e72266fa4f1c
Alireza Savand <alireza.savand@gmail.com>
2013-08-08 16:02:40 +0400

    Implement basic setter/getter methods on DownloadHolder

--- download/downloadholder.cpp
+++ download/downloadholder.cpp
@@ -4,3 +4,103 @@ DownloadHolder::DownloadHolder(QObject *parent) :
     QObject(parent)
 {
 }
+
+QString DownloadHolder::uuid() const
+{
+    return _uuid;
+}
+
+void DownloadHolder::setUuid(const QString &uuid)
+{
+    _uuid = uuid;
+}
+
+QString DownloadHolder::url() const
+{
+    return _url;
+}
+
+void DownloadHolder::setUrl(const QString &url)
+{
+    _url = url;
+}
+
+QString DownloadHolder::savePath() const
+{
+    return _savePath;
+}
+
+void DownloadHolder::setSavePath(const QString &savePath)
+{
+    _savePath = savePath;
+}
+
+QString DownloadHolder::fileName() const
+{
+    return _fileName;
+}
+
+void DownloadHolder::setFileName(const QString &fileName)
+{
+    _fileName = fileName;
+}
+
+QString DownloadHolder::status() const
+{
+    return _status;
+}
+
+void DownloadHolder::setStatus(const QString &status)
+{
+    _status = status;
+}
+
+QString DownloadHolder::progress() const
+{
+    return _progress;
+}
+
+void DownloadHolder::setProgress(const QString &progress)
+{
+    _progress = progress;
+}
+
+QString DownloadHolder::remainingTime() const
+{
+    return _remainingTime;
+}
+
+void DownloadHolder::setRemainingTime(const QString &remainingTime)
+{
+    _remainingTime = remainingTime;
+}
+
+QString DownloadHolder::speed() const
+{
+    return _speed;
+}
+
+void DownloadHolder::setSpeed(const QString &speed)
+{
+    _speed = speed;
+}
+
+QString DownloadHolder::added() const
+{
+    return _added;
+}
+
+void DownloadHolder::setAdded(const QString &added)
+{
+    _added = added;
+}
+
+QString DownloadHolder::downloaded() const
+{
+    return _downloaded;
+}
+
+void DownloadHolder::setDownloaded(const QString &downloaded)
+{
+    _downloaded = downloaded;
+}
--- download/downloadholder.h
+++ download/downloadholder.h
@@ -9,6 +9,48 @@ class DownloadHolder : public QObject
 public:
     explicit DownloadHolder(QObject *parent = 0);
 
+    QString uuid() const;
+    void setUuid(const QString &uuid);
+
+    QString url() const;
+    void setUrl(const QString &url);
+
+    QString savePath() const;
+    void setSavePath(const QString &savePath);
+
+    QString fileName() const;
+    void setFileName(const QString &fileName);
+
+    QString status() const;
+    void setStatus(const QString &status);
+
+    QString progress() const;
+    void setProgress(const QString &progress);
+
+    QString remainingTime() const;
+    void setRemainingTime(const QString &remainingTime);
+
+    QString speed() const;
+    void setSpeed(const QString &speed);
+
+    QString added() const;
+    void setAdded(const QString &added);
+
+    QString downloaded() const;
+    void setDownloaded(const QString &downloaded);
+
+private:
+    QString _uuid;
+    QString _url;
+    QString _savePath;
+    QString _fileName;
+    QString _status;
+    QString _progress;
+    QString _remainingTime;
+    QString _speed;
+    QString _added;
+    QString _downloaded;
+
 };
 
 #endif // DOWNLOADHOLDER_H

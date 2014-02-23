5f82b2e08c94ad8a3dcebf19828e4d6e5a5c586e
Alireza Savand <alireza.savand@gmail.com>
2014-02-23 16:16:55 +0400

    Update changelog and version number

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
     a.setApplicationName(QObject::tr("yoDownet"));
-    a.setApplicationVersion("2014.02.22.1");
+    a.setApplicationVersion("2014.02.23");
     a.setOrganizationName("Alir3z4");
     a.setOrganizationDomain("yodownet.sourceforge.net");
 

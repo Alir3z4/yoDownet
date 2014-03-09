b6595e14014c60063e4f7a28e2463ddc2e1b5d44
Alireza Savand <alireza.savand@gmail.com>
2014-03-09 21:50:25 +0400

    Better pointer/variable naming

--- downloader/yodownet.cpp
+++ downloader/yodownet.cpp
@@ -86,8 +86,10 @@ void yoDownet::removeDownload(const QUuid &uuid)
 void yoDownet::replyMetaDataChanged(QObject *currentReply)
 {
     QHash<QNetworkReply*, Download*>::iterator i = _downloadHash->find(qobject_cast<QNetworkReply*>(currentReply));
-    Status *status = _statusHash->find(i.key()->url()).value();
-    status->setBytesTotal(i.key()->header(QNetworkRequest::ContentLengthHeader).toULongLong());
+    QNetworkReply *reply = i.key();
+    Status *status = _statusHash->find(reply->url()).value();
+
+    status->setBytesTotal(reply->header(QNetworkRequest::ContentLengthHeader).toULongLong());
 }
 
 void yoDownet::startRequest(Download *newDownload)
